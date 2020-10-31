803a96ad676d3a5d5ae7e766073d422895f0b602
beecoder <sdweb@ro.ru>
2020-10-31 18:08:17 +0300

    close #11

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,33 +1,8 @@
-type UserCheckListener = (isActive: boolean) => void
+import { CancableTimeout } from './core/lib'
 
-class UserActive {
-  isActive: boolean
-  timer?: number
-  listener: UserCheckListener
-  constructor() {
-    this.isActive = true
-    this.timer = undefined
-    this.listener = () => {}
-  }
 
-  onChange(checkListener: UserCheckListener) {
-    this.listener = checkListener
-  }
+const userActive = new CancableTimeout(7 * 60 * 1000)
 
-  active() {
-    if (this.isActive === false) {
-      this.isActive = true
-      this.listener(this.isActive)
-    }
-    window.clearTimeout(this.timer)
-    this.timer = window.setTimeout(() => {
-      this.isActive = false
-      this.listener(this.isActive)
-    }, 7 * 60 * 1000)
-  }
-}
-
-const userActive = new UserActive()
 
 userActive.onChange((isActive) => {
   var msg = {type: 'history_visit_isActive', payload: isActive}
@@ -36,5 +11,5 @@ userActive.onChange((isActive) => {
 
 
 document.addEventListener('mousemove', function () {
-  userActive.active()
+  userActive.update()
 })
